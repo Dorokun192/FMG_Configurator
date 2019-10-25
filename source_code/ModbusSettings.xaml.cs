7cca7f3159167213c70a455aceb1f1ef39d1c757
Dorokun192 <52736483+Dorokun192@users.noreply.github.com>
2019-10-25 13:17:34 +0800

    full source code

---
+++ source_code/ModbusSettings.xaml.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.IO.Ports;
+using static FMG_Configurator__csharp_.ModbusCard;
+
+namespace FMG_Configurator__csharp_
+{
+    /// <summary>
+    /// Interaction logic for ModbusSettings.xaml
+    /// </summary>
+    public partial class ModbusSettings : Window
+    {
+        #region Declarations
+        public MainWindow mainWindow = new MainWindow();
+        public static string type;
+        public SerialPort serportModbus = new SerialPort();
+        public static int counter = 1;
+        #endregion
+
+        public ModbusSettings()
+        {
+            InitializeComponent();
+            Dev1ModbusCard.SaveButtonClicked += new EventHandler(Dev1Handler);
+            Dev2ModbusCard.SaveButtonClicked += new EventHandler(Dev2Handler);
+            Dev3ModbusCard.SaveButtonClicked += new EventHandler(Dev3Handler);
+            Dev4ModbusCard.SaveButtonClicked += new EventHandler(Dev4Handler);
+
+            Dev2ModbusCard.ComboBoxChanged += new EventHandler(Dev2Combo);
+            Dev3ModbusCard.ComboBoxChanged += new EventHandler(Dev3Combo);
+            Dev4ModbusCard.ComboBoxChanged += new EventHandler(Dev4Combo);
+
+        }
+        #region Window Events
+        private void modbusWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            modbusWin.Title = "Modbus Settings - " + Globals.devID;
+
+            Dev1ModbusCard.DeviceNameTextBlock.Text = "Device01";
+            Dev2ModbusCard.DeviceNameTextBlock.Text = "Device02";
+            Dev3ModbusCard.DeviceNameTextBlock.Text = "Device03";
+            Dev4ModbusCard.DeviceNameTextBlock.Text = "Device04";
+
+            FreeTagsTextBlock.Text = Globals.TagCount.ToString();
+
+        }
+
+        private void modbusWin_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            serportModbus.Close();
+            SerialClose.IsEnabled = false;
+            SerialOpen.IsEnabled = true;
+            mainWindow.Show();
+            
+            Visibility = Visibility.Collapsed;
+        }
+       
+        #endregion
+
+        #region Button Events
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string ID1 = Dev1ModbusCard.NodeIDTextBox.Text;
+            string ID2 = Dev2ModbusCard.NodeIDTextBox.Text;
+            string ID3 = Dev3ModbusCard.NodeIDTextBox.Text;
+            string ID4 = Dev4ModbusCard.NodeIDTextBox.Text;
+
+            string[] a = { ID1, ID2, ID3, ID4 };
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (a[i] != "")
+                {
+                    for (int j = 1; j < 3; j++)
+                    {
+                        if (a[i + j] != "")
+                            if (a[i] == a[i + j])
+                            {
+                                MessageBox.Show("Similar Node ID found" + a[i] + a[i + j]);
+                                return;
+                            }
+                    }
+                }
+            }
+
+            //send RTU settings data
+            SendData(30, RTUBaudRate.Text);
+            SendData(31, RTUBit.Text);
+            SendData(32, RTUParity.Text);
+            SendData(33, RTUPollTime.Text);
+            SendData(34, RTUTimeout.Text);
+
+            if (Dev1ModbusCard.TagSelection.IsEnabled & ID1 != "")
+            {
+                SendData(35, ID1);
+
+                switch (Dev1ModbusCard.DAQTagTypeComboBox.Text)
+                {
+                    case "Modbus Coils(0x)":
+                        type = "1";
+                        break;
+                    case "Discrete Inputs(1x)":
+                        type = "2";
+                        break;
+                    case "Input Registers(3x)":
+                        type = "3";
+                        break;
+                    case "Holding Registers(4x)":
+                        type = "4";
+                        break;
+                    default:
+                        break;
+                }
+
+                SendData(36, type);
+                TagSend(36, Dev1ModbusCard);
+            }
+
+            if (Dev2ModbusCard.TagSelection.IsEnabled & ID2 != "")
+            {
+                SendData(54, ID2);
+                switch (Dev2ModbusCard.DAQTagTypeComboBox.Text)
+                {
+                    case "Modbus Coils(0x)":
+                        type = "1";
+                        break;
+                    case "Discrete Inputs(1x)":
+                        type = "2";
+                        break;
+                    case "Input Registers(3x)":
+                        type = "3";
+                        break;
+                    case "Holding Registers(4x)":
+                        type = "4";
+                        break;
+                    default:
+                        break;
+                }
+                SendData(55, type);
+                TagSend(55, Dev2ModbusCard);
+            }
+
+            if (Dev3ModbusCard.TagSelection.IsEnabled & ID3 != "")
+            {
+                SendData(73, ID3);
+
+                switch (Dev3ModbusCard.DAQTagTypeComboBox.Text)
+                {
+                    case "Modbus Coils(0x)":
+                        type = "1";
+                        break;
+                    case "Discrete Inputs(1x)":
+                        type = "2";
+                        break;
+                    case "Input Registers(3x)":
+                        type = "3";
+                        break;
+                    case "Holding Registers(4x)":
+                        type = "4";
+                        break;
+                    default:
+                        break;
+                }
+                SendData(74, type);
+                TagSend(74, Dev3ModbusCard);
+            }
+
+            if (Dev4ModbusCard.TagSelection.IsEnabled & ID4 != "")
+            {
+                //SendData(54, ID4);
+                //SendData(55, Dev2ModbusCard.DAQTagTypeComboBox.Text);
+                //foreach (var x in Dev2ModbusCard.TagSelection.Items.OfType<User>().ToList())
+                //{
+                //    if (x.IsSelected)
+                //    {
+                //        TagLeft -= 1;
+                //        SendData(55 + x.TagAddress, x.TagAddress.ToString());
+                //    }// send id, and name to device?
+                //}
+                //Dev4ModbusCard.TagSelection.IsReadOnly = true;
+            }
+            FreeTagsTextBlock.Text = Globals.TagCount.ToString();
+            MessageBox.Show("sent to device successful");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dev1ModbusCard.TagSelection.IsEnabled = Dev1ModbusCard.TagSelection.IsReadOnly = false;
+            Dev1ModbusCard.TagSelection.ItemsSource = null;
+            Dev2ModbusCard.TagSelection.IsEnabled = Dev2ModbusCard.TagSelection.IsReadOnly = false;
+            Dev2ModbusCard.TagSelection.ItemsSource = null;
+            Dev3ModbusCard.TagSelection.IsEnabled = Dev3ModbusCard.TagSelection.IsReadOnly = false;
+            Dev3ModbusCard.TagSelection.ItemsSource = null;
+            Dev4ModbusCard.TagSelection.IsEnabled = Dev4ModbusCard.TagSelection.IsReadOnly = false;
+            Dev4ModbusCard.TagSelection.ItemsSource = null;
+            Dev1ModbusCard.CardSaveButton.IsEnabled = true;
+            Dev2ModbusCard.CardSaveButton.IsEnabled = true;
+            Dev3ModbusCard.CardSaveButton.IsEnabled = true;
+            Dev4ModbusCard.CardSaveButton.IsEnabled = true;
+            Globals.TagCount = 64;
+            FreeTagsTextBlock.Text = Globals.TagCount.ToString();
+
+            // reset tag addresses
+            for (int i = 30; i <= 100; i++)
+            {
+                SendData(i, "");
+            }
+            MessageBox.Show("reset all data");
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            // reset tag addresses
+            for (int i = 30; i <= 100; i++)
+            {
+                SendData(i, "");
+            }
+
+            Dev1ModbusCard.TagSelection.IsReadOnly = false;
+            Dev2ModbusCard.TagSelection.IsReadOnly = false;
+            Dev3ModbusCard.TagSelection.IsReadOnly = false;
+            Dev4ModbusCard.TagSelection.IsReadOnly = false;
+            Dev1ModbusCard.CardSaveButton.IsEnabled = true;
+            Dev2ModbusCard.CardSaveButton.IsEnabled = true;
+            Dev3ModbusCard.CardSaveButton.IsEnabled = true;
+            Dev4ModbusCard.CardSaveButton.IsEnabled = true;
+
+            Globals.TagCount = 64;
+            FreeTagsTextBlock.Text = Globals.TagCount.ToString();
+        }
+
+
+        private void SerialOpen_Click(object sender, RoutedEventArgs e)
+        {
+            serportModbus.PortName = Globals.comport;
+            serportModbus.BaudRate = int.Parse(Globals.baudrate);
+            switch (Globals.uparity)
+            {
+                case "None":
+                    serportModbus.Parity = Parity.None;
+                    break;
+                case "Odd":
+                    serportModbus.Parity = Parity.Odd;
+                    break;
+                case "Even":
+                    serportModbus.Parity = Parity.Even;
+                    break;
+                default:
+                    break;
+            }
+            serportModbus.DataBits = int.Parse(Globals.bit);
+            if (Globals.ustopbit == "1")
+                serportModbus.StopBits = StopBits.One;
+            else if (Globals.ustopbit == "2")
+                serportModbus.StopBits = StopBits.Two;
+
+            serportModbus.Open();
+
+            SerialClose.IsEnabled = true;
+            SaveButton.IsEnabled = true;
+            EditButton.IsEnabled = true;
+            ResetButton.IsEnabled = true;
+            SerialOpen.IsEnabled = false;
+        }
+
+        private void SerialClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (serportModbus.IsOpen)
+            {
+                serportModbus.Write($"\r{500}*END\n");
+                serportModbus.Close();
+
+                SerialClose.IsEnabled = false;
+                SaveButton.IsEnabled = false;
+                EditButton.IsEnabled = false;
+                ResetButton.IsEnabled = false;
+                SerialOpen.IsEnabled = true;
+            }
+        }
+
+        private void AddCardButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (counter)
+            {
+                case 1:
+                    Dev1ModbusCard.Visibility = Visibility.Visible;
+                    counter++;
+                    break;
+                case 2:
+                    if (Dev1ModbusCard.TagSelection.IsReadOnly)
+                    {
+                        Dev2ModbusCard.Visibility = Visibility.Visible;
+                        counter++;
+                    }
+                    else
+                        MessageBox.Show("Save previous Modbus first!", "Save MODBUS Card", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                    
+                case 3:
+                    if (Dev2ModbusCard.TagSelection.IsReadOnly)
+                    {
+                        Dev3ModbusCard.Visibility = Visibility.Visible;
+                        counter++;
+                    }
+                    else
+                        MessageBox.Show("Save previous Modbus first!", "Save MODBUS Card", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                case 4:
+                    if (Dev3ModbusCard.TagSelection.IsReadOnly)
+                    {
+                        Dev4ModbusCard.Visibility = Visibility.Visible;
+                        counter++;
+                    }
+                    else
+                        MessageBox.Show("Save previous Modbus first!", "Save MODBUS Card", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                default:
+                    MessageBox.Show("Maximum number of devices reached!", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+            }
+            
+        }
+        #endregion
+
+        #region Card Handler Events
+
+        private void Dev1Handler(object sender, EventArgs e) => TagSelect(Dev1ModbusCard);
+        private void Dev2Handler(object sender, EventArgs e) => TagSelect(Dev2ModbusCard);
+        private void Dev3Handler(object sender, EventArgs e) => TagSelect(Dev3ModbusCard);
+        private void Dev4Handler(object sender, EventArgs e) => TagSelect(Dev4ModbusCard);
+
+        private void Dev2Combo(object sender, EventArgs e)
+        {
+            if (!Dev1ModbusCard.TagSelection.IsReadOnly)
+            {
+                MessageBox.Show("Please save previous MODBUS first.","Unsaved MODBUS", MessageBoxButton.OK,MessageBoxImage.Warning);
+                return;
+            }
+        }
+        private void Dev3Combo(object sender, EventArgs e)
+        {
+            if (!Dev2ModbusCard.TagSelection.IsReadOnly)
+            {
+                MessageBox.Show("Please save previous MODBUS first.", "Unsaved MODBUS", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+        }
+        private void Dev4Combo(object sender, EventArgs e)
+        {
+            if (!Dev3ModbusCard.TagSelection.IsReadOnly)
+            {
+                MessageBox.Show("Please save previous MODBUS first.", "Unsaved MODBUS", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+        }
+
+        private void TagSelect(ModbusCard modbus)
+        {
+            if (!modbus.TagSelection.IsEnabled)
+            {
+                MessageBox.Show("Please select tag type");
+                return;
+            }
+            foreach (var x in modbus.TagSelection.Items.OfType<User>().ToList())
+                if (x.IsSelected)
+                    Globals.TagCount--;
+
+            modbus.TagSelection.IsReadOnly = true;
+            FreeTagsTextBlock.Text = Globals.TagCount.ToString();
+            modbus.CardSaveButton.IsEnabled = false;
+        }
+        #endregion
+
+        #region Data Send Events
+        private void TagSend(int address, ModbusCard modbus)
+        {
+            foreach (var x in modbus.TagSelection.Items.OfType<User>().ToList())
+                if (x.IsSelected)
+                    SendData(address + x.TagAddress, x.TagAddress.ToString());
+        }
+        private void SendData(int ID, string content) => serportModbus.Write($"\r{ID}*{content}\n");
+        #endregion
+       
+    }
+}
---
+++ source_code/NetSettings.xaml.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Windows;
+using System.IO.Ports;
+
+
+namespace FMG_Configurator__csharp_
+{
+    /// <summary>
+    /// Interaction logic for NetSettings.xaml
+    /// </summary>
+    public partial class NetSettings : Window
+    {
+        public static MainWindow mainWindow = new MainWindow();
+        public static SerialPort serportNet = new SerialPort();
+        
+        // temporary storage for values to be sent to the device
+        // for now the storage is up to 100 due to device memory limitation
+        public static string[] fmg = new string[101];
+
+
+        public NetSettings() => InitializeComponent();
+
+        // ================= BEGIN Window Event METHODS ================================================= //
+        private void NetSettingsWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DeviceIDTextBlock.Text = Globals.devID;
+            NetSettingsWindow.Title = "Network Settings - " + Globals.devID;
+        } 
+
+        private void NetSettingsWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+
+            if (serportNet.IsOpen)
+                serportNet.Close();
+
+            Visibility = Visibility.Collapsed;
+            mainWindow.Show();
+        }
+        // ================= END Window Event METHODS =================================================== //
+
+        // ================= BEGIN CHECK Event METHODS ================================================== //
+        private void loadData()
+        {
+            LanIPTextBox.LoadIPAddress(fmg[1]);
+            LanSubnetMaskTextBox.LoadIPAddress(fmg[2]);
+            LanGatewayTextBox.LoadIPAddress(fmg[3]);
+            LanDNS1TextBox.LoadIPAddress(fmg[4]);
+            LanDNS2TextBox.LoadIPAddress(fmg[5]);
+
+            WlanPassTextBox.Text = fmg[6];
+            WlanSSIDTextBox.Text = fmg[7];
+            WlanIPTextBox.LoadIPAddress(fmg[8]);
+            WlanSubnetMaskTextBox.LoadIPAddress(fmg[9]);
+            WlanGatewayTextBox.LoadIPAddress(fmg[10]);
+            WlanDNS1TextBox.LoadIPAddress(fmg[11]);
+            WlanDNS2TextBox.LoadIPAddress(fmg[12]);
+
+            MobileIPTextBox.LoadIPAddress(fmg[13]);
+            MobileSubnetMaskTextBox.LoadIPAddress(fmg[14]);
+            MobileGatewayTextBox.LoadIPAddress(fmg[15]);
+            MobileDNS1TextBox.LoadIPAddress(fmg[16]);
+            MobileDNS2TextBox.LoadIPAddress(fmg[17]);
+            MobileUsernameTextBox.Text = fmg[19];
+            MobilePasswordTextBox.Text = fmg[20];
+            MobileAPNTextBox.Text = fmg[21];
+            
+            MQTTPassTextBox.Text = fmg[23];
+            LwtTopicTextBox.Text = fmg[24];
+            LwtMessTextBox.Text = fmg[25];
+            LwtRetTextBox.Text = fmg[26];
+            LwtQOSTextBox.Text = fmg[27];
+        }
+
+        private void MobileOptionalCheckBox_Checked(object sender, RoutedEventArgs e) => MobileOptionalGrid.IsEnabled = true;
+
+        private void MobileOptionalCheckBox_Unchecked(object sender, RoutedEventArgs e) => MobileOptionalGrid.IsEnabled = false;
+
+        private void LDHCPDis_Checked(object sender, RoutedEventArgs e) => LDHCPGrid.IsEnabled = true;
+
+        private void LDHCPEn_Checked(object sender, RoutedEventArgs e) => LDHCPGrid.IsEnabled = false;
+
+        private void LDNSDis_Checked(object sender, RoutedEventArgs e) => LDNSGrid.IsEnabled = true;
+
+        private void LAutoDNSOn_Checked(object sender, RoutedEventArgs e) => LDNSGrid.IsEnabled = false;
+
+        private void WDHCPDis_Checked(object sender, RoutedEventArgs e) => WDHCPGrid.IsEnabled = true;
+
+        private void WDNSOff_Checked(object sender, RoutedEventArgs e) => WDNSGrid.IsEnabled = true;
+
+        private void WDHCPEn_Checked(object sender, RoutedEventArgs e) => WDHCPGrid.IsEnabled = false;
+
+        private void WDNSOn_Checked(object sender, RoutedEventArgs e) => WDNSGrid.IsEnabled = false;
+
+        private void MQTTLWTCheckBox_Checked(object sender, RoutedEventArgs e) => MQTTLWTGrid.IsEnabled = true;
+
+        private void MQTTLWTCheckBox_Unchecked(object sender, RoutedEventArgs e) => MQTTLWTGrid.IsEnabled = false;
+        // ================= END CHECK Event METHODS =================================================== //
+
+        // ================= BEGIN Serial Event METHODS ========================================= //
+        private void SendStrings(int a, int b)
+        {
+            for (int i = a; i <= b; i++)
+                if (fmg[i] != "")
+                    serportNet.Write($"\r{i}*{fmg[i]}\n");
+        }
+        // ================= END Serial Event METHODS =========================================== //
+
+        // ================= BEGIN SettingsApply (with globals) METHODS =============================================== //
+        private void LanSettingsApply()
+        {
+            if (LDHCPGrid.IsEnabled)
+            {
+                fmg[1] = LanIPTextBox.GetString();
+                fmg[2] = LanSubnetMaskTextBox.GetString();
+                fmg[3] = LanGatewayTextBox.GetString();
+
+                SendStrings(1, 3);
+            }
+
+            if (LDNSGrid.IsEnabled)
+            {
+                fmg[4] = LanDNS1TextBox.GetString();
+                fmg[5] = LanDNS2TextBox.GetString();
+
+                SendStrings(4, 5);
+            }
+        }
+
+        private void WlanSettingsApply()
+        {
+            if (WlanSSIDTextBox.Text == "" | WlanPassTextBox.Text == "")
+            {
+                MessageBox.Show("missing Password or SSID data");
+                return;
+            }
+            else
+            {
+                fmg[28] = WlanPassTextBox.Text;
+                fmg[7] = WlanSSIDTextBox.Text;
+
+                SendStrings(7, 7);
+                SendStrings(28, 28);
+            }
+
+            if (WDHCPGrid.IsEnabled)
+            {
+                fmg[8] = WlanIPTextBox.GetString();
+                fmg[9] = WlanSubnetMaskTextBox.GetString();
+                fmg[10] = WlanGatewayTextBox.GetString();
+
+                SendStrings(8, 10);
+            }
+
+            if (WDNSGrid.IsEnabled)
+            {
+                fmg[11] = WlanDNS1TextBox.GetString();
+                fmg[12] = WlanDNS2TextBox.GetString();
+
+                SendStrings(11, 12);
+            }
+        }
+
+        private void MobileSettingsApply()
+        {
+            if (MobileOptionalGrid.IsEnabled)
+            {
+                fmg[13] = MobileIPTextBox.GetString();
+                fmg[14] = MobileSubnetMaskTextBox.GetString();
+                fmg[15] = MobileGatewayTextBox.GetString();
+                fmg[16] = MobileDNS1TextBox.GetString();
+                fmg[17] = MobileDNS2TextBox.GetString();
+                fmg[18] = MobileAuthenticateComboBox.Text;
+                fmg[19] = MobileUsernameTextBox.Text;
+                fmg[20] = MobilePasswordTextBox.Text;
+
+                SendStrings(13, 20);
+            }
+            fmg[21] = MobileAPNTextBox.Text;
+
+            SendStrings(21, 21);
+        }
+
+        private void MQTTSettingsApply()
+        {
+            int stringtointeger;
+            
+
+            if (analogdelTextBox.Text == "")
+            {
+                MessageBox.Show("no value for analog time");
+                return;
+            }
+            stringtointeger = Int32.Parse(analogdelTextBox.Text);
+
+            if (timerCombo.Text == "second")
+                stringtointeger *= 1000;
+            else if (timerCombo.Text == "minute")
+                stringtointeger *= 60000;
+            else
+                stringtointeger *= 3600000;
+
+            fmg[22] = stringtointeger.ToString();
+            fmg[23] = MQTTPassTextBox.Text;
+
+            if (MQTTLWTGrid.IsEnabled)
+            {
+                fmg[24] = LwtTopicTextBox.Text;
+                fmg[25] = LwtMessTextBox.Text;
+                fmg[26] = LwtRetTextBox.Text;
+                fmg[27] = LwtQOSTextBox.Text;
+
+                SendStrings(24, 27);
+            }
+            SendStrings(22, 23);
+        }
+
+        // ================= END SettingsApply (with globals) METHODS ================================================= //
+
+        // ================= BEGIN Button Click Event METHODS ========================================== //
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (LanTab.IsSelected)
+            {
+                LanSettingsApply();
+                MessageBox.Show("LAN Settings applied");
+            }
+            else if (WlanTab.IsSelected)
+            {
+                WlanSettingsApply();
+                MessageBox.Show("WLAN Settings applied");
+            }
+            else if (MobileTab.IsSelected)
+            {
+                MobileSettingsApply();
+                MessageBox.Show("Mobile Settings applied");
+            }
+            else if (MQTTTab.IsSelected)
+            {
+                MQTTSettingsApply();
+                MessageBox.Show("MQTT settings applied");
+            }
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            LanSettingsApply();
+            WlanSettingsApply();
+            MobileSettingsApply();
+            MQTTSettingsApply();
+
+            MessageBox.Show("All settings applied");
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Show();
+            Close();
+        }
+        // ================= END Button Click Event METHODS ============================================ //
+
+        // ================= BEGIN Menu Click Event METHODS ========================================== //
+        private void LoadDevData_Click(object sender, RoutedEventArgs e) => loadData();
+
+        private void ResetDevData_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure to reset all data?", "Reset Alert", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                for (int i = 1; i < 101; i++)
+                    serportNet.Write($"\r{i}*\n");
+
+                MessageBox.Show("all data has been erased");
+            }
+            else if (result == MessageBoxResult.No)
+                return;
+        }
+
+        private void SerialOpen_Click(object sender, RoutedEventArgs e)
+        {
+            serportNet.PortName = Globals.comport;
+            serportNet.BaudRate = int.Parse(Globals.baudrate);
+
+            if (Globals.uparity == "None")
+                serportNet.Parity = Parity.None;
+            else if (Globals.uparity == "Even")
+                serportNet.Parity = Parity.Even;
+            else
+                serportNet.Parity = Parity.Odd;
+
+            serportNet.DataBits = int.Parse(Globals.bit);
+
+            if (Globals.ustopbit == "1")
+                serportNet.StopBits = StopBits.One;
+            else
+                serportNet.StopBits = StopBits.Two;
+
+            serportNet.Open();
+
+            NetworkTabControl.IsEnabled = true;
+            SerialOpen.IsEnabled = false;
+            SerialClose.IsEnabled = true;
+            ApplyButton.IsEnabled = true;
+            OKButton.IsEnabled = true;
+            CloseButton.IsEnabled = true;
+        }
+
+        private void SerialClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (serportNet.IsOpen)
+            {
+                serportNet.Write($"\r{500}*END\n");
+                serportNet.Close();
+            }
+
+            NetworkTabControl.IsEnabled = false;
+            SerialOpen.IsEnabled = true;
+            SerialClose.IsEnabled = false;
+            ApplyButton.IsEnabled = false;
+            OKButton.IsEnabled = false;
+            CloseButton.IsEnabled = false;
+        }
+        // ================= END Menu Click Event METHODS ========================================== //
+
+    }
+}
